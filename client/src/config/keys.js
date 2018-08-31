d4f84031cc587516cd792ebfa4ae104883d88491
weeznog <drew.m.peterson@gmail.com>
2018-08-31 14:07:48 -0700

    fix query-string bug ??

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -1,11 +1,19 @@
-export default (function() {
-  if (process.env.NODE_ENV === "development") {
-    return {
-      GRAPHCOOL_URI: process.env.REACT_APP_GRAPHCOOL_DOMAIN,
-      DOMAIN: process.env.REACT_APP_DOMAIN,
-      AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
-      AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
-      AUTH0_HOST: process.env.REACT_APP_AUTH0_HOST
-    };
-  }
-})();
+// export default (function() {
+//   if (process.env.NODE_ENV === "development") {
+//     return {
+//       GRAPHCOOL_URI: process.env.REACT_APP_GRAPHCOOL_DOMAIN,
+//       DOMAIN: process.env.REACT_APP_DOMAIN,
+//       AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
+//       AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+//       AUTH0_HOST: process.env.REACT_APP_AUTH0_HOST
+//     };
+//   }
+// })();
+
+export default {
+  GRAPHCOOL_URI: process.env.REACT_APP_GRAPHCOOL_DOMAIN,
+  DOMAIN: process.env.REACT_APP_DOMAIN,
+  AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN,
+  AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID,
+  AUTH0_HOST: process.env.REACT_APP_AUTH0_HOST
+};
